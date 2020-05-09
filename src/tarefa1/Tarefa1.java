8f0c9d700ae22f66757f2bd41a4f1664713025c5
Lalo <aalvarezvbretana>
2020-05-09 17:13:51 +0200

    acabado

---
+++ src/tarefa1/Tarefa1.java
@@ -0,0 +1,19 @@
+package tarefa1;
+
+/**
+ *
+ * @author Lalo
+ */
+public class Tarefa1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        Componentes vis = new Componentes();
+        
+    }
+    
+}
